b8246b225cb6774aec1cc600a13a53113cfb8da8
kamrul <kamrulhassan1361@gmail.com>
2017-12-01 19:15:06 +0600

    Database class created and all connection is ok with it

--- admin/includes/new_config.php
+++ admin/includes/new_config.php
@@ -6,12 +6,6 @@ define('DB_USER','root');
 define('DB_PASS','');
 define('DB_NAME','gallery_db');
 
-$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
-
-if($connection){
-	echo "true";
-}
-
 
 
 
