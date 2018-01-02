bf256f4dd2d28b030802d6abc6a34fc79cb6c4ea
kamrul <kamrulhassan1361@gmail.com>
2018-01-02 19:37:19 +0600

    delete method created in users class to delete a user from db

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -142,6 +142,16 @@ public function update(){
 }
 
 
+public function delete(){
+	global $database;
+
+	$sql = "DELETE FROM users ";
+	$sql .= "WHERE id =" . $database->escape_string($this->id);
+	$sql .= " LIMIT 1";
+	$database->query($sql);
+	return (mysqli_affected_rows($database->connection) == 1) ? true : false;
+}
+
 
 
 
